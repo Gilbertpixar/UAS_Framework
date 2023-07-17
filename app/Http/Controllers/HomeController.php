e6b1ba6e65a755e7e432f49139a0e40caa0f9e5f
Gilbertpixar <gilbertpierro18@gmail.com>
2023-07-17 14:54:09 +0700

    second commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,4 +7,21 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+
+    function index()
+    {
+        $pageTitle = 'Home';
+
+        return view('home', ['pageTitle' => $pageTitle]);
+    }
 }
